3c00dbf13caf960fc6e6ed6eba045b156e4cb5e8
Joao Aroeira <j.aroeira@gmail.com>
2023-09-18 00:10:23 +0000

    add user controller endpoints - no implementation yet

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,14 @@
 from flask import Flask
+from flask_smorest import Api
 from config import get_config
+from .controllers.user import blp as UserBlueprint 
 
 
 def create_app() -> Flask:
     app = Flask(__name__)
     app.config.from_object(get_config())
+    api = Api(app)
+
+    api.register_blueprint(UserBlueprint)
 
     return app
---
+++ app/controllers/user.py
@@ -0,0 +1,84 @@
+from flask.views import MethodView
+from flask_smorest import Blueprint
+
+blp = Blueprint("User", "user", description="User api endpoints", url_prefix="/user")
+
+@blp.route("/signup")
+class UserSignup(MethodView):
+
+    def post(self):
+        return {"message": "user signup endpoint - TODO"}, 201
+    
+@blp.route("/verify-email")
+class UserVerifyEmail(MethodView):
+
+    def get(self):
+        return {"message": "user verify email endpoint - TODO"}, 200
+
+
+@blp.route("/signin")
+class UserSignin(MethodView):
+
+    def post(self):
+        return {"message": "user signin endpoint - TODO"}, 200
+    
+
+@blp.route("/refresh-token")
+class UserRefreshToken(MethodView):
+
+    def post(self):
+        return {"message": "user refresh token endpoint - TODO"}, 200
+    
+
+@blp.route("/revoke-refresh-token")
+class UserRevokeRefreshToken(MethodView):
+
+    def post(self):
+        return {"message": "user revoke refresh token endpoint - TODO"}, 200
+    
+@blp.route("/forgot-password")
+class UserForgotPassword(MethodView):
+
+    def post(self):
+        return {"message": "user forgot password endpoint - TODO"}, 200
+    
+@blp.route("/reset-password")
+class UserResetPassword(MethodView):
+
+    def post(self):
+        return {"message": "user reset password endpoint - TODO"}, 200
+    
+@blp.route("/change-password")
+class UserChangePassword(MethodView):
+
+    def put(self):
+        return {"message": "user change password endpoint - TODO"}, 200
+    
+
+
+# Admin only endpoints
+
+@blp.route("/")
+class User(MethodView):
+
+    def get(self):
+        """Get a list of all users - Admin only"""
+        return {"message": "get a list with all users - endpoint - TODO"}, 200
+
+    def post(self):
+        """Create user - Admin only"""
+        return {"message": "create user endpoint - TODO"}, 201
+    
+    def put(self):
+        """Update user - Admin only"""
+        return {"message": "update user endpoint - TODO"}, 200
+    
+
+@blp.route("/<int:user_id>")
+class UserById(MethodView):
+
+    def get(self, user_id):
+        return {"message": f"get user by id endpoint - TODO - id passed: {user_id}"}, 200
+    
+    def delete(self, user_id):
+        return {"message": f"delete user by id endpoint - TODO - id passed: {user_id}"}, 200
