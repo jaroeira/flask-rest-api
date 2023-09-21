d2d223cea7636393329ae74dc72621aa77837fc8
Joao Aroeira <j.aroeira@gmail.com>
2023-09-21 21:11:46 +0000

    fix: EmailVerificationTokenDto - deprecaded meta

--- app/dtos/auth_dto.py
+++ app/dtos/auth_dto.py
@@ -2,7 +2,4 @@ from marshmallow import Schema, fields
 
 
 class EmailVerificationTokenDto(Schema):
-    token = fields.String(
-        required=True,
-        description="Verification Token sent to the users email"
-    )
+    token = fields.Str(required=True)
